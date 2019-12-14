1a0c88b4a942bb296999d7646ffc5c56ee01e281
barelampagos <barelampagos@gmail.com>
2019-12-14 11:49:31 -0800

    Refactoring to Stateless functional components

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,9 +1,9 @@
 var React = require('react');
 
-var About = React.createClass({
-	render: function() {
-		return <h3>About Component</h3>;
-	}
-});
+// Stateless functional component
+// Can only do when Component only has 'render' method
+var About = props => {
+	return <h3>About Component</h3>;
+};
 
 module.exports = About;
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,15 +1,11 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-	render: function() {
-		var { location, temp } = this.props;
-
-		return (
-			<p>
-				It's {temp} in {location}
-			</p>
-		);
-	}
-});
+var WeatherMessage = ({ temp, location }) => {
+	return (
+		<p>
+			It's {temp} in {location}.
+		</p>
+	);
+};
 
 module.exports = WeatherMessage;
