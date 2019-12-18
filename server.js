a9f1df02426c46e9a61359e099cb7f1d5a075ce7
barelampagos <barelampagos@gmail.com>
2019-12-17 21:08:22 -0800

    Prep for Heroku

--- server.js
+++ server.js
@@ -2,10 +2,23 @@ var express = require('express');
 
 // Create app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+// Express Middleware - lets us handle incoming req's
+// req - Incoming request
+// res - What gets sent back
+// next - Call this when done
+app.use(function(req, res, next) {
+	if (req.headers['x-forwarded-proto'] === 'http') {
+		next();
+	} else {
+		res.redirect('http://' + req.hostname + req.url);
+	}
+});
 
 app.use(express.static('public'));
 
 // Start server
-app.listen(3000, function () {
-    console.log('Express server is up on port 3000')
+app.listen(PORT, function() {
+	console.log('Express server is up on PORT ' + PORT);
 });
