d33318ef80d357b948a6f0bfde722c4f8e27be82
barelampagos <barelampagos@gmail.com>
2019-12-18 21:28:32 -0800

    Updating About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,12 @@ var React = require('react');
 // Stateless functional component
 // Can only do when Component only has 'render' method
 var About = props => {
-	return <h3>About Component</h3>;
+	return (
+		<div>
+			<h3>About</h3>
+			<p>Welcome to About Page</p>
+		</div>
+	);
 };
 
 module.exports = About;
